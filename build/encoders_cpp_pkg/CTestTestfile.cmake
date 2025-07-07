4fc1db46d0df29a4c08372075326f8a686a34b20
MelonFox85 <vanyapoddaev@gmail.com>
2025-07-07 21:56:48 +0300

    test

--- build/encoders_cpp_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/airat/ros2_cpp_ws/src/encoders_cpp_pkg
-# Build directory: /home/airat/ros2_cpp_ws/src/build/encoders_cpp_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/motor_controller_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/airat/ros2_cpp_ws/src/motor_controller_cpp
-# Build directory: /home/airat/ros2_cpp_ws/src/build/motor_controller_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
